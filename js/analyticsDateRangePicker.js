c118a7d5ad63cdd28ebaf4887dde063a7d34a3d4
rexthonyy <rexthonyy@gmail.com>
2021-04-21 15:51:22 +0100

    Created the sticky nav and content

---
+++ js/analyticsDateRangePicker.js
@@ -0,0 +1,324 @@
+const customRangeLabel = "Custom Range";
+let isWindowClickable = false;
+class AnalyticsDateRangePicker {
+	constructor(analyticsDateRangePickerElement, options){
+		this.analyticsDateRangePickerElement = analyticsDateRangePickerElement;
+		this.model = new AnalyticsDateRangePickerModel(options, this.updateView.bind(this));
+
+		this.isShown = false;
+		this.onUpdateListener = ()=>{};
+
+		this.updateView();
+		this.toggleDropdownVisibility();
+	}
+
+	updateView(){
+		this.createView();
+		this.createMenuItems();
+		this.setMenuItemSelection();
+		this.setClickListener();
+	}
+
+	createView(){
+		let elementWidth = this.analyticsDateRangePickerElement.offsetWidth + "px";
+		let elementHeight = this.analyticsDateRangePickerElement.offsetHeight + "px";
+
+		this.analyticsDateRangePickerElement.innerHTML = `
+		<div id="ADRP_AnalyticsDateRangePickerContainer">
+			<div id="ADRP_DropdownClickHandler" style="height: ${elementHeight};">
+				<div id="ADRP_DisplayTextContainer">
+					<span id="ADRP_DropdownIcon"><img src="images/icons/ic_dropdown_arrow.png" class="rex-width-16px rex-height-16px rex-vertical-align"></span>
+					<span id="ADRP_DisplayText">${this.model.getSelectedMenuDisplayLabel()}</span>
+				</div>
+				<div id="ADRP_DateRangePicker">
+					<input id="ADRP_StartDateInput" class="ADRP_DateInput" type="text" />
+					<input id="ADRP_EndDateInput" class="ADRP_DateInput" type="text"/>
+				</div>
+			</div>
+
+			<div id="ADRP_DropdownContainer">
+				<div id="ADRP_DropdownMenuContainer" style="width: ${elementWidth};">
+
+				</div>
+			</div>
+		</div>
+		`;
+	}
+
+	createMenuItems(){
+		let innerHTML = "";
+
+		this.model.options.menus.forEach(menu => {
+			innerHTML += `
+			<div class="ADRP_MenuItem">
+			${menu.menuLabel}
+			</div>
+			`;
+		});
+
+		document.getElementById("ADRP_DropdownMenuContainer").innerHTML = innerHTML;
+	}
+
+	setMenuItemSelection(){
+		let menuItemElems = document.getElementsByClassName("ADRP_MenuItem");
+
+		for(let i = 0; i < this.model.options.menus.length; i++){
+			if(this.model.options.menus[i].isSelected){
+				menuItemElems[i].className = "ADRP_MenuItem ADRP_MenuItemSelected";
+			}else{
+				menuItemElems[i].className = "ADRP_MenuItem";
+			}
+		}
+	}
+
+	setClickListener(){
+		this.setElementClickListener();
+		this.setMenuItemClickListener();
+		this.setCalendarItemClickListener();
+	}
+
+	setElementClickListener(){
+		document.getElementById("ADRP_DropdownClickHandler").onclick = (e) => {
+			this.stopClickPropagation(e);
+			this.toggleDropdownVisibility();
+		};
+	}
+
+	toggleDropdownVisibility(){
+		let dropdownContainer = document.getElementById("ADRP_DropdownContainer");
+		if(this.isShown){
+			dropdownContainer.style.display = "block";
+			isWindowClickable = true;
+			// window.onclick = () => {
+			// 	this.toggleDropdownVisibility();
+			// };
+		}else{
+			dropdownContainer.style.display = "none";
+			isWindowClickable = false;
+			//window.onclick = () => {};
+			//console.log(document.getElementById("ADRP_StartDateInput").value);
+		}
+
+		this.isShown = !this.isShown;
+	}
+
+	stopClickPropagation(e){
+		if(!e) e = window.event;
+		if(e.stopPropagation){
+			e.stopPropagation();
+		}else{
+			e.cancelBubble = true;
+		}
+	}
+
+	setMenuItemClickListener(){
+		let menuItemElems = document.getElementsByClassName("ADRP_MenuItem");
+
+		let menuItemElemsLength = menuItemElems.length;
+		for(let i = 0; i < menuItemElemsLength - 1; i++){
+			menuItemElems[i].onclick = (e) => {
+				//this.stopClickPropagation(e);
+				this.onUpdateListener(this.model.options.menus[i].getRange());
+				this.model.selectMenuItem(i);
+				this.updateView();
+			};
+		}
+
+		//click the custom range
+		menuItemElems[menuItemElemsLength - 1].onclick = () => {
+			this.selectionCount = 0;
+			this.isSwapDates = false;
+			this.isManualSelection = false;
+			let elem = document.getElementById("ADRP_DateRangePicker");
+			this.dateRangePicker = new DateRangePicker(elem, { });
+			let range = this.model.getSelectedMenuDisplayRange();
+			this.dateRangePicker.setDates(range.startDate, range.endDate);
+
+			this.isManualSelection = true;
+			document.getElementById("ADRP_StartDateInput").focus();
+		};
+	}
+
+	setCalendarItemClickListener(){
+		const startDateInput = document.getElementById("ADRP_StartDateInput");
+		const endDateInput = document.getElementById("ADRP_EndDateInput");
+
+		this.onInputChange(startDateInput, (val) => {
+			if(this.isManualSelection){
+				if(!this.isSwapDates){
+					
+					let selectedDate = (new Date(val)).getTime();
+					let currentEndDate = this.model.options.endDate.getTime();
+
+					if(selectedDate < currentEndDate){
+						if(this.selectionCount == 0){
+							endDateInput.focus();
+						}else{
+							let dates = this.dateRangePicker.getDates();
+
+							let time0 = dates[0].getTime();
+							let time1 = dates[1].getTime();
+
+							if(time0 > time1){
+								let temp = dates[0];
+								dates[0] = dates[1];
+								dates[1] = temp;
+							}
+
+							this.model.options.startDate = dates[0];
+							this.model.options.endDate = dates[1];
+
+							this.dateRangePicker.destroy();
+
+							let customMenuIndex = this.model.options.menus.length - 1;
+							this.onUpdateListener(this.model.options.menus[customMenuIndex].getRange());
+							this.model.selectMenuItem(customMenuIndex);
+							this.toggleDropdownVisibility();
+							this.updateView();
+						}
+					}else{
+						this.selectionCount++;
+						if(this.selectionCount == 1){
+							this.isSwapDates = true;
+						}else{
+							let dates = this.dateRangePicker.getDates();
+
+							let time0 = dates[0].getTime();
+							let time1 = dates[1].getTime();
+
+							if(time0 > time1){
+								let temp = dates[0];
+								dates[0] = dates[1];
+								dates[1] = temp;
+							}
+
+							this.model.options.startDate = dates[0];
+							this.model.options.endDate = dates[1];
+
+							this.dateRangePicker.destroy();
+
+							let customMenuIndex = this.model.options.menus.length - 1;
+							this.onUpdateListener(this.model.options.menus[customMenuIndex].getRange());
+							this.model.selectMenuItem(customMenuIndex);
+							this.toggleDropdownVisibility();
+							this.updateView();
+						}
+					}
+				}
+			}
+		});
+
+		this.onInputChange(endDateInput, (val) => {
+			if(this.isManualSelection){
+				if(!this.isSwapDates){
+
+					let dates = this.dateRangePicker.getDates();
+
+					let time0 = dates[0].getTime();
+					let time1 = dates[1].getTime();
+
+					if(time0 > time1){
+						let temp = dates[0];
+						dates[0] = dates[1];
+						dates[1] = temp;
+					}
+
+					this.model.options.startDate = dates[0];
+					this.model.options.endDate = dates[1];
+
+					this.dateRangePicker.destroy();
+
+					let customMenuIndex = this.model.options.menus.length - 1;
+					this.onUpdateListener(this.model.options.menus[customMenuIndex].getRange());
+					this.model.selectMenuItem(customMenuIndex);
+					this.toggleDropdownVisibility();
+					this.updateView();
+				}else{
+					this.isSwapDates = false;
+				}
+			}
+		});
+	}
+
+	onInputChange(inputElm, callback){
+		const {get, set} = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value');
+		Object.defineProperty(inputElm, 'value', {
+			get() {
+				return get.call(this);
+			},
+			set(newVal){
+				callback(newVal);
+				return set.call(this, newVal);
+			}
+		});
+	}
+}
+
+function handleDateSelectorDropdown(event){
+	if(isWindowClickable){
+		dateRangePicker.toggleDropdownVisibility();
+	}
+}
+
+class AnalyticsDateRangePickerModel {
+	constructor(options, callback){
+		this.callback = callback;
+		this.options = options;
+		
+		this.addCustomRange();
+		this.selectMenuItem(this.options.menus.length - 1);
+	}
+
+	addCustomRange(){
+		this.options.menus.push({
+			menuLabel: customRangeLabel,
+			getDisplayLabel: () => {
+
+				let startDay = this.options.startDate.getDate();
+				let startMonth = this.options.startDate.getMonth() + 1;
+				let startYear = this.options.startDate.getFullYear();
+
+				let endDay = this.options.endDate.getDate();
+				let endMonth = this.options.endDate.getMonth() + 1;
+				let endYear = this.options.endDate.getFullYear();
+
+
+				//return startDay + "/" + startMonth + "/" + startYear + " - " + endDay + "/" + endMonth + "/" + endYear;
+				return startDay + " " + this.options.startDate.toLocaleString('default', { month: 'short' }) + " - " + endDay + " " + this.options.endDate.toLocaleString('default', { month: 'short' });
+			},
+			getRange: () => {
+				return {
+					startDate: this.options.startDate,
+					endDate: this.options.endDate
+				};
+			}
+		});
+	}
+
+	getSelectedMenuDisplayLabel(){
+		for(let i = 0; i < this.options.menus.length; i++){
+			if(this.options.menus[i].isSelected){
+				return this.options.menus[i].getDisplayLabel();
+			}
+		}
+	}
+
+	getSelectedMenuDisplayRange(){
+		for(let i = 0; i < this.options.menus.length; i++){
+			if(this.options.menus[i].isSelected){
+				return this.options.menus[i].getRange();
+			}
+		}
+	}
+
+	deselectAllMenuItems(){
+		this.options.menus.forEach(menu => {
+			menu.isSelected = false;
+		});
+	}
+
+	selectMenuItem(index){
+		this.deselectAllMenuItems();
+		this.options.menus[index].isSelected = true;
+	}
+}
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,178 @@
-let navPos = 0;
-window.onscroll = () => {
-	let header = document.getElementById("stickyNav");
-	let stickyPos = header.offsetTop;
-	if(stickyPos > navPos){
-		navPos = stickyPos;
+window.onload = () => {
+	let settingsDropdownToggle = document.getElementById("settingsDropdownToggle");
+	let settingsDropdown = document.getElementById("settingsDropdown");
+	let dashboardSiteDropdownToggle = document.getElementById("dashboardSiteDropdownToggle");
+	let dashboardSiteDropdown = document.getElementById("dashboardSiteDropdown");
+
+	/* When the user clicks on the button,
+	toggle between hiding and showing the dropdown content */
+	settingsDropdownToggle.onclick = (e) => {
+		stopClickPropagation(e);
+		settingsDropdown.classList.toggle("rex-cd-show");
+	};
+	dashboardSiteDropdownToggle.onclick = (e) => {
+		stopClickPropagation(e);
+		dashboardSiteDropdown.classList.toggle("rex-cd-show");
+	};
+	
+	// Close the dropdown menu if the user clicks outside of it
+	window.onclick = function(event) {
+		handleSettingsDropdown(event);
+		handleSiteDropdown(event);
+		handleDateSelectorDropdown(event);
 	}
-	if (window.pageYOffset > navPos) {
-		header.classList.add("custom-sticky");
-		header.classList.add("rex-box-shadow2");
-	} else {
-		header.classList.remove("custom-sticky");
-		header.classList.remove("rex-box-shadow2");
+
+	function handleSettingsDropdown(event){
+		if (!event.target.matches('.rex-cd-dropbtn')) {
+			let dropdowns = document.getElementsByClassName("rex-cd-dropdown-content");
+			for (let i = 0; i < dropdowns.length; i++) {
+				var openDropdown = dropdowns[i];
+				if (openDropdown.classList.contains('rex-cd-show')) {
+					openDropdown.classList.remove('rex-cd-show');
+				}
+			}			
+		}
 	}
+
+	function handleSiteDropdown(event){
+		if (!event.target.matches('.rex-cd-dropbtn')) {
+			let dropdowns = document.getElementsByClassName("rex-cd-dropdown-content-min");
+			for (let i = 0; i < dropdowns.length; i++) {
+				var openDropdown = dropdowns[i];
+				if (openDropdown.classList.contains('rex-cd-show')) {
+					openDropdown.classList.remove('rex-cd-show');
+				}
+			}
+		}
+	}
+
+	
+	let navPos = 0;
+	window.onscroll = () => {
+		let header = document.getElementById("stickyNav");
+		let stickyPos = header.offsetTop;
+		if(stickyPos > navPos){
+			navPos = stickyPos;
+		}
+		if (window.pageYOffset > navPos) {
+			header.classList.add("custom-sticky");
+			header.classList.add("rex-box-shadow2");
+		} else {
+			header.classList.remove("custom-sticky");
+			header.classList.remove("rex-box-shadow2");
+		}
+	};
+
+	var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+				'rgba(255, 99, 132, 1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+
+
+
+
+
+	let myPicker = document.getElementById("myPicker");
+	dateRangePicker = new AnalyticsDateRangePicker(myPicker, {
+		startDate: new Date("01/08/2021"),
+		endDate: new Date("01/20/2021"),
+		menus: [
+		{
+			menuLabel: 'Today',
+			getDisplayLabel: () => {
+				return 'Today'
+			},
+			getRange: () => {
+				return {
+					startDate: new Date("01/01/2021"),
+					endDate: new Date()
+				};
+			}
+		},
+		{
+			menuLabel: 'Last 7 days',
+			getDisplayLabel: () => {
+				return 'Last 7 days'
+			},
+			getRange: () => {
+				return {
+					startDate: new Date("01/01/2021"),
+					endDate: new Date()
+				};
+			}
+		},
+		{
+			menuLabel: 'Last 30 days',
+			getDisplayLabel: () => {
+				return 'Last 30 days'
+			},
+			getRange: () => {
+				return {
+					startDate: new Date("01/05/2018"),
+					endDate: new Date()
+				};
+			}
+		},
+		{
+			menuLabel: 'Last 6 months',
+			getDisplayLabel: () => {
+				return 'Last 6 months'
+			},
+			getRange: () => {
+				return {
+					startDate: new Date("01/05/2018"),
+					endDate: new Date()
+				};
+			}
+		},
+		{
+			menuLabel: 'Last 12 months',
+			getDisplayLabel: () => {
+				return 'Last 12 months'
+			},
+			getRange: () => {
+				return {
+					startDate: new Date("01/05/2018"),
+					endDate: new Date()
+				};
+			}
+		}
+		]
+	});
+
+	dateRangePicker.onUpdateListener = response => {
+		console.log(response.startDate);
+		console.log(response.endDate);
+	};
 };
