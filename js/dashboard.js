bcca8d80f905f5b830f8e7afc2e2b43739929367
rexthonyy <rexthonyy@gmail.com>
2021-04-22 16:32:43 +0100

    chart responsiveness done

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,8 @@ window.onload = () => {
 	let settingsDropdown = document.getElementById("settingsDropdown");
 	let dashboardSiteDropdownToggle = document.getElementById("dashboardSiteDropdownToggle");
 	let dashboardSiteDropdown = document.getElementById("dashboardSiteDropdown");
+	let dashboardFilterDropdownToggle = document.getElementById("dashboardFilterDropdownToggle");
+	let dashboardFilterDropdown = document.getElementById("dashboardFilterDropdown");
 
 	/* When the user clicks on the button,
 	toggle between hiding and showing the dropdown content */
@@ -14,11 +16,15 @@ window.onload = () => {
 		stopClickPropagation(e);
 		dashboardSiteDropdown.classList.toggle("rex-cd-show");
 	};
+	dashboardFilterDropdownToggle.onclick = (e) => {
+		stopClickPropagation(e);
+		dashboardFilterDropdown.classList.toggle("rex-cd-show");
+	};
 	
 	// Close the dropdown menu if the user clicks outside of it
 	window.onclick = function(event) {
 		handleSettingsDropdown(event);
-		handleSiteDropdown(event);
+		handleMinDropdown(event);
 		handleDateSelectorDropdown(event);
 	}
 
@@ -34,7 +40,7 @@ window.onload = () => {
 		}
 	}
 
-	function handleSiteDropdown(event){
+	function handleMinDropdown(event){
 		if (!event.target.matches('.rex-cd-dropbtn')) {
 			let dropdowns = document.getElementsByClassName("rex-cd-dropdown-content-min");
 			for (let i = 0; i < dropdowns.length; i++) {
@@ -64,44 +70,101 @@ window.onload = () => {
 	};
 
 	var ctx = document.getElementById('myChart').getContext('2d');
-	var myChart = new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-				'rgba(255, 99, 132, 1)',
-				'rgba(54, 162, 235, 1)',
-				'rgba(255, 206, 86, 1)',
-				'rgba(75, 192, 192, 1)',
-				'rgba(153, 102, 255, 1)',
-				'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
-			}]
-		},
-		options: {
-			scales: {
-				y: {
-					beginAtZero: true
+	var myChart = null;
+	function createChart(){
+		myChart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [{
+					label: '# of Votes',
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
 				}
 			}
+		});
+	}
+
+	createChart();
+
+	let resizeValue = -1;
+
+
+	function displayWindowSize(){
+		var w = document.documentElement.clientWidth;
+		var h = document.documentElement.clientHeight;
+
+		//console.log(`width : ${w} height : ${h}`);
+
+		if(w < 384){
+			if(resizeValue != 4){
+				resizeValue = 4;
+				myChart.destroy();
+				ctx.canvas.height = 230;
+				createChart();
+				console.log("chart updated 4");
+			}
+		}else if(w < 500){
+			if(resizeValue != 3){
+				resizeValue = 3;
+				myChart.destroy();
+				ctx.canvas.height = 200;
+				createChart();
+				console.log("chart updated 3");
+			}			
+		}else if(w < 775){
+			if(resizeValue != 2){
+				resizeValue = 2;
+				myChart.destroy();
+				ctx.canvas.height = 170;
+				createChart();
+				console.log("chart updated 2");
+			}
+		}else if(w < 950){
+			if(resizeValue != 1){
+				resizeValue = 1;
+				myChart.destroy();
+				ctx.canvas.height = 140;
+				createChart();
+				console.log("chart updated 1");
+			}
+		}else if(w < 1000){
+			if(resizeValue != 0){
+				resizeValue = 0;
+				myChart.destroy();
+				ctx.canvas.height = 110;
+				createChart();
+				console.log("chart updated 0");
+			}
 		}
-	});
+	}
 
+	window.addEventListener("resize", displayWindowSize);
 
-
-
+	displayWindowSize();
 
 	let myPicker = document.getElementById("myPicker");
 	dateRangePicker = new AnalyticsDateRangePicker(myPicker, {
